00f966136554a4a7eac9a610dd819a28deacaec1
RyBuilds <Ryan@Registe.me>
2022-07-30 11:31:09 +0100

    first

--- src/todoist.ts
+++ src/todoist.ts
@@ -1,12 +1,15 @@
-
 import axios from "axios";
 
-export const exchangeCode = async (code: String, client_id: String, client_secret: String): Promise<String> => {
+export const exchangeCode = async (
+  code: String,
+  client_id: String,
+  client_secret: String
+): Promise<String> => {
   const url = "https://todoist.com/oauth/access_token";
-  const headers = {"Content-Type": "application/json"};
-  const body = {client_id, client_secret, code}
+  const headers = { "Content-Type": "application/json" };
+  const body = { client_id, client_secret, code };
 
-  const response = await axios.post(url, body, {headers} );
+  const response = await axios.post(url, body, { headers });
 
   if (!response.data || response.status !== 200) {
     throw new Error("Non 200 status received when fetching recently played.");
